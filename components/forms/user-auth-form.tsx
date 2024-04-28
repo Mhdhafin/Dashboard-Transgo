931761386e077f5c0d715f6323bb45f7ae927603
arisdanang <arisdanang2810@gmail.com>
2024-04-29 02:01:01 +0700

    feat: implement and hit login api

--- components/forms/user-auth-form.tsx
+++ components/forms/user-auth-form.tsx
@@ -15,10 +15,10 @@ import { useSearchParams } from "next/navigation";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
-import GoogleSignInButton from "../github-auth-button";
 
 const formSchema = z.object({
   email: z.string().email({ message: "Enter a valid email address" }),
+  password: z.string(),
 });
 
 type UserFormValue = z.infer<typeof formSchema>;
@@ -27,17 +27,15 @@ export default function UserAuthForm() {
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get("callbackUrl");
   const [loading, setLoading] = useState(false);
-  const defaultValues = {
-    email: "demo@gmail.com",
-  };
+
   const form = useForm<UserFormValue>({
     resolver: zodResolver(formSchema),
-    defaultValues,
   });
 
   const onSubmit = async (data: UserFormValue) => {
     signIn("credentials", {
       email: data.email,
+      password: data.password,
       callbackUrl: callbackUrl ?? "/dashboard",
     });
   };
@@ -68,22 +66,29 @@ export default function UserAuthForm() {
             )}
           />
 
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="Enter your password..."
+                    disabled={loading}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
           <Button disabled={loading} className="ml-auto w-full" type="submit">
-            Continue With Email
+            Login
           </Button>
         </form>
       </Form>
-      <div className="relative">
-        <div className="absolute inset-0 flex items-center">
-          <span className="w-full border-t" />
-        </div>
-        <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-background px-2 text-muted-foreground">
-            Or continue with
-          </span>
-        </div>
-      </div>
-      <GoogleSignInButton />
     </>
   );
 }
--- lib/auth-options.ts
+++ lib/auth-options.ts
@@ -1,35 +1,73 @@
 import { NextAuthOptions } from "next-auth";
-import GithubProvider from "next-auth/providers/github";
 import CredentialProvider from "next-auth/providers/credentials";
 
 export const authOptions: NextAuthOptions = {
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID ?? "",
-      clientSecret: process.env.GITHUB_SECRET ?? "",
-    }),
     CredentialProvider({
+      name: "credentials",
       credentials: {
         email: {
           label: "email",
           type: "email",
           placeholder: "example@gmail.com",
         },
+        password: {
+          label: "password",
+          type: "password",
+        },
       },
       async authorize(credentials, req) {
-        const user = { id: "1", name: "John", email: credentials?.email };
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user;
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
-          return null;
+        if (typeof credentials !== "undefined") {
+          const res = await fetch(
+            `${process.env.NEXT_PUBLIC_API_HOST}/auth/login`,
+            {
+              method: "POST",
+              body: JSON.stringify(credentials),
+              headers: {
+                "Content-Type": "application/json",
+              },
+            },
+          );
 
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+          const user = await res.json();
+
+          if (res.ok && user) {
+            console.log("user", user.data.user);
+            // Any object returned will be saved in `user` property of the JWT
+            return user.data.user;
+          } else {
+            // If you return null then an error will be displayed advising the user to check their details.
+            return null;
+
+            // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+          }
         }
       },
     }),
   ],
+  callbacks: {
+    async session({ session, token }: { session: any; token: any }) {
+      if (token) {
+        session.user.email = token.email;
+        session.user.name = token.name;
+        session.user.id = token.id;
+        session.user.access_token = token.access_token;
+        session.user.role = token.role;
+      }
+      return session;
+    },
+    async jwt({ token, user }: { token: any; user: any }) {
+      if (user) {
+        token.email = user.email;
+        token.name = user.name;
+        token.id = user.id;
+        token.access_token = user.access_token;
+        token.role = user.role;
+      }
+      return token;
+    },
+  },
+  // session: { strategy: "jwt" },
   pages: {
     signIn: "/", //sigin page
   },
