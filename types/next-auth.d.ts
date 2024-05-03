38e638ccf8372a23f13868e9d6ed6efa1c068041
arisdanang <arisdanang2810@gmail.com>
2024-05-04 00:37:32 +0700

    fix: extend type next-auth

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
@@ -8,6 +8,6 @@ declare module "next-auth" {
       role: string;
       accessToken: string;
       email: string;
-    };
+    } & DefaultSession["user"];
   }
 }
