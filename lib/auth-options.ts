33aeca2366023b85422fc8928ed6d83016748c68
arisdanang <arisdanang2810@gmail.com>
2024-05-06 00:22:01 +0700

    fix: remove console

--- lib/auth-options.ts
+++ lib/auth-options.ts
@@ -17,33 +17,39 @@ export const authOptions: NextAuthOptions = {
         },
       },
       async authorize(credentials, req) {
-        if (typeof credentials !== "undefined") {
-          const res = await fetch(
-            `${process.env.NEXT_PUBLIC_API_HOST}/auth/login`,
-            {
-              method: "POST",
-              body: JSON.stringify(credentials),
-              headers: {
-                "Content-Type": "application/json",
+        try {
+          if (typeof credentials !== "undefined") {
+            const res = await fetch(
+              `${process.env.NEXT_PUBLIC_API_HOST}/auth/login`,
+              {
+                method: "POST",
+                body: JSON.stringify(credentials),
+                headers: {
+                  "Content-Type": "application/json",
+                },
               },
-            },
-          );
+            );
 
-          const user = await res.json();
+            const user = await res.json();
 
-          if (!res) {
-            throw new Error("error login");
+            if (!res) {
+              throw new Error("error login");
+            }
+
+            if (res.ok && user) {
+              console.log("masukkk");
+              // Any object returned will be saved in `user` property of the JWT
+              return user.data;
+            } else {
+              console.log("erorr");
+              // If you return null then an error will be displayed advising the user to check their details.
+              return null;
+
+              // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+            }
           }
-
-          if (res.ok && user) {
-            // Any object returned will be saved in `user` property of the JWT
-            return user.data;
-          } else {
-            // If you return null then an error will be displayed advising the user to check their details.
-            return null;
-
-            // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-          }
+        } catch (error) {
+          throw new Error("error cuyy");
         }
       },
     }),
@@ -62,6 +68,7 @@ export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
   session: { strategy: "jwt" },
   pages: {
-    signIn: "/", //sigin page
+    signIn: "/", //sigin page,
+    error: "/",
   },
 };
