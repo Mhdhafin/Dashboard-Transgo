9aeaae92482a462abc5035ca9d4862cdfec38211
arisdanang <arisdanang2810@gmail.com>
2024-05-06 00:24:11 +0700

    feat: delete driver

--- components/tables/driver-tables/cell-action.tsx
+++ components/tables/driver-tables/cell-action.tsx
@@ -8,7 +8,11 @@ import {
   DropdownMenuLabel,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { toast } from "@/components/ui/use-toast";
 import { Drivers } from "@/constants/data";
+import { useDeleteDriver } from "@/hooks/api/useDriver";
+import useAxiosAuth from "@/hooks/axios/use-axios-auth";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Edit, MoreHorizontal, Trash } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import { useState } from "react";
@@ -21,8 +25,56 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
   const [loading, setLoading] = useState(false);
   const [open, setOpen] = useState(false);
   const router = useRouter();
+  const queryClient = useQueryClient();
+  const id = data?.id;
+  console.log("data?.id", data?.id);
+  const { mutateAsync: deleteDriver } = useDeleteDriver(id);
+  const axiosAuth = useAxiosAuth();
+  // const {mutate: deleteDriver} = useDeleteDriver(id)
+  // console.log("cek", data);
+  // const deleteDriver = () => {
+  //   return axiosAuth.delete(`drivers/${id}`);
+  // };
+  // const onConfirm = async () => {
+  //   try {
+  //     await deleteDriverMutation.mutateAsync(id);
+  //   } catch (error) {
+  //     toast({
+  //       variant: "destructive",
+  //       title: "gagal menghapus driver",
+  //     });
+  //   }
+  // };
 
-  const onConfirm = async () => {};
+  // const deleteDriverMutation = useMutation({
+  //   mutationFn: deleteDriver,
+  //   onSuccess: () => {
+  //     queryClient.invalidateQueries({ queryKey: ["drivers"] });
+  //     toast({
+  //       variant: "success",
+  //       title: "Driver berhasil dihapus",
+  //     });
+  //   },
+  // });
+
+  const onConfirm = async () => {
+    deleteDriver(data?.id, {
+      onSuccess: () => {
+        toast({
+          variant: "success",
+          title: "berhasill",
+        });
+      },
+      onError: (error) => {
+        toast({
+          variant: "destructive",
+          title: "ada error cuyy",
+          description: `something went wrong: ${error.message}`,
+        });
+        queryClient.invalidateQueries({ queryKey: ["drivers"] });
+      },
+    });
+  };
 
   return (
     <>
@@ -43,7 +95,7 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
           <DropdownMenuLabel>Actions</DropdownMenuLabel>
 
           <DropdownMenuItem
-            onClick={() => router.push(`/dashboard/user/${data.id}`)}
+            onClick={() => router.push(`/dashboard/driver/${data?.id}`)}
           >
             <Edit className="mr-2 h-4 w-4" /> Update
           </DropdownMenuItem>
--- hooks/api/useDriver.ts
+++ hooks/api/useDriver.ts
@@ -1,11 +1,12 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import useAxiosAuth from "../axios/use-axios-auth";
 
+const baseEndpoint = "/drivers";
 export const useGetDriver = (params: any) => {
   const axiosAuth = useAxiosAuth();
 
   const getDrivers = () => {
-    return axiosAuth.get("/drivers", {
+    return axiosAuth.get(baseEndpoint, {
       params,
     });
   };
@@ -18,12 +19,32 @@ export const useGetDriver = (params: any) => {
 
 export const usePostDriver = () => {
   const axiosAuth = useAxiosAuth();
+  const queryClient = useQueryClient();
 
   const postDriver = (body: any) => {
-    return axiosAuth.post("/drivers", body);
+    return axiosAuth.post(baseEndpoint, body);
   };
 
   return useMutation({
     mutationFn: postDriver,
+    onMutate: async () => {
+      await queryClient.cancelQueries({ queryKey: ["drivers"] });
+    },
+  });
+};
+
+export const useDeleteDriver = (id: number) => {
+  const axiosAuth = useAxiosAuth();
+  const queryClient = useQueryClient();
+
+  const deleteUserFn = async (id: number) => {
+    return axiosAuth.delete(`${"driver"}/${id}`);
+  };
+
+  return useMutation({
+    mutationFn: deleteUserFn,
+    onMutate: async () => {
+      await queryClient.cancelQueries({ queryKey: ["drivers"] });
+    },
   });
 };
