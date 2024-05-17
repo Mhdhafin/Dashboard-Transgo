9f645ab4154b03fdb9c2f8e3d8873706675a81b4
arisdanang <arisdanang2810@gmail.com>
2024-05-17 14:27:59 +0700

    fix: spinner color

--- components/spinner.tsx
+++ components/spinner.tsx
@@ -6,7 +6,7 @@ const Spinner = () => {
       <div role="status">
         <svg
           aria-hidden="true"
-          className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-white"
+          className="inline w-8 h-8 text-gray-200 animate-spin fill-black  dark:text-gray-600 dark:fill-white"
           viewBox="0 0 100 101"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
