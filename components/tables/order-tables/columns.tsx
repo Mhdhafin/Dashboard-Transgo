5f8af001f05942bf22a058ce1e857c7fd2e7cc62
arisdanang <arisdanang2810@gmail.com>
2024-07-03 00:57:30 +0700

    fix: completed column

--- components/tables/order-tables/columns.tsx
+++ components/tables/order-tables/columns.tsx
@@ -144,8 +144,73 @@ export const onProgressColumns: ColumnDef<any>[] = [
     cell: ({ row }) => <span>{formatRupiah(row.original.total_price)}</span>,
   },
   {
-    accessorKey: "status",
+    id: "actions",
+    cell: ({ row }) => <CellAction data={row.original} />,
+  },
+];
+
+export const completedColumns: ColumnDef<any>[] = [
+  {
+    accessorKey: "name",
+    header: "Pelanggan",
+    cell: ({ row }) => <span>{row.original.customer.name}</span>,
+  },
+  {
+    accessorKey: "fleet",
+    header: "Armada",
+    cell: ({ row }) => <span>{row.original.fleet.name}</span>,
+  },
+  {
+    accessorKey: "duration",
+    header: "Waktu",
+    cell: ({ row }) => (
+      <HoverCard>
+        <HoverCardTrigger className="bg-[#f5f5f5] rounded-full py-1 px-3">
+          {row.original.duration} hari
+        </HoverCardTrigger>
+        <HoverCardContent>
+          <div className="flex items-center">
+            <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
+            <span className="text-muted-foreground font-normal text-[12px] leading-4">
+              Tanggal Pengambilan
+            </span>
+          </div>
+          <div className="pt-1">
+            <p className="text-[14px] font-semibold leading-5">
+              senin, 24 juni 2024
+            </p>
+            <p className="text-[14px] font-normal leading-5">Jam 17:00</p>
+          </div>
+          <Separator className="my-4" />
+          <div className="flex items-center">
+            <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
+            <span className="text-muted-foreground font-normal text-[12px] leading-4">
+              Tanggal Pengembilan
+            </span>
+          </div>
+          <div className="pt-1">
+            <p className="text-[14px] font-semibold leading-5">
+              senin, 24 juni 2024
+            </p>
+            <p className="text-[14px] font-normal leading-5">Jam 17:00</p>
+          </div>
+        </HoverCardContent>
+      </HoverCard>
+    ),
+  },
+  {
+    accessorKey: "pic",
+    header: "Penanggung Jawab",
+    cell: ({ row }) => <span>{row.original.end_request.driver.name}</span>,
+  },
+  {
+    accessorKey: "total_price",
     header: "Total Harga",
+    cell: ({ row }) => <span>{formatRupiah(row.original.total_price)}</span>,
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
     cell: ({ row }) => (
       <>
         {row.original.request_status === "pending" ? (
@@ -159,83 +224,6 @@ export const onProgressColumns: ColumnDef<any>[] = [
         )}
       </>
     ),
-  },
-  {
-    id: "actions",
-    cell: ({ row }) => <CellAction data={row.original} />,
-  },
-];
-
-export const completedColumns: ColumnDef<any>[] = [
-  {
-    accessorKey: "name",
-    header: "Customer",
-    cell: ({ row }) => <span>{row.original.customer.name}</span>,
-  },
-  {
-    accessorKey: "type",
-    header: "Tipe Tasks",
-    cell: ({ row }) => {
-      const getType = () => {
-        if (
-          row.original.type == "delivery" &&
-          row.original.is_self_pickup == false
-        )
-          return "Pengantaran";
-        if (
-          row.original.type == "delivery" &&
-          row.original.is_self_pickup == true
-        )
-          return "Pengambilan";
-        if (
-          row.original.type == "pick_up" &&
-          row.original.is_self_pickup == false
-        )
-          return "Penjemputan";
-        if (
-          row.original.type == "pick_up" &&
-          row.original.is_self_pickup == true
-        )
-          return "Pengembalian";
-      };
-
-      return <span>{getType()}</span>;
-    },
-  },
-  {
-    accessorKey: "fleet.name",
-    header: "Nama Kendaraan",
-    cell: ({ row }) => (
-      <span>
-        {row.original.fleet.name} (
-        {row.original.fleet.type == "car" ? "Mobil" : "Motor"})
-      </span>
-    ),
-  },
-  {
-    accessorKey: "time",
-    header: "Waktu",
-    cell: ({ row }) => (
-      <span>
-        {dayjs(row.original.start_date)
-          .locale("id")
-          .format("dddd, D MMMM YYYY - HH:mm:ss")}
-      </span>
-    ),
-  },
-  {
-    accessorKey: "driver.name",
-    header: "PIC",
-    cell: ({ row }) => <span>{row.original.driver.name}</span>,
-  },
-  {
-    accessorKey: "driver.name",
-    header: "Durasi",
-    cell: ({ row }) => {
-      return (
-        <span>{convertTime(row?.original?.progress_duration_second)}</span>
-      );
-    },
   },
   {
     id: "actions",
