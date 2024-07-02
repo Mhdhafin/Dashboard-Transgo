b4e07fa4e0d76444d1371ace96198e35ff610666
arisdanang <arisdanang2810@gmail.com>
2024-07-03 00:56:15 +0700

    fix: edit page

--- app/(dashboard)/dashboard/orders/[orderId]/edit/page.tsx
+++ app/(dashboard)/dashboard/orders/[orderId]/edit/page.tsx
@@ -5,26 +5,27 @@ import Spinner from "@/components/spinner";
 import { useGetDetailOrder } from "@/hooks/api/useOrder";
 import { useSidebar } from "@/hooks/useSidebar";
 import { cn } from "@/lib/utils";
-import React, { useEffect } from "react";
+import React from "react";
 
 export default function Page({ params }: { params: { orderId: number } }) {
-    useEffect(() => { });
-    const { isMinimized } = useSidebar();
+  const { isMinimized } = useSidebar();
 
-    const breadcrumbItems = [
-        { title: "Pesanan", link: "/dashboard/orders" },
-        { title: "Tinjau Pesanan", link: "/dashboard/orders/detail" },
-    ];
+  const breadcrumbItems = [
+    { title: "Pesanan", link: "/dashboard/orders" },
+    { title: "Tinjau Pesanan", link: "/dashboard/orders/detail" },
+  ];
 
-    const { data, isFetching } = useGetDetailOrder(params.orderId);
+  const { data, isFetching } = useGetDetailOrder(params.orderId);
 
-    return (
-        <div className="flex-1 space-y-4 p-5 pr-0">
-            <div className={cn(isMinimized ? "w-[936px]" : "w-[700px]")}>
-                <BreadCrumb items={breadcrumbItems} />
-            </div>
-            {isFetching && <Spinner />}
-            {!isFetching && data?.data && <OrderForm initialData={data?.data} />}
-        </div>
-    );
+  return (
+    <div className="flex-1 space-y-4 p-5 pr-0">
+      <div className={cn(isMinimized ? "w-[936px]" : "w-[700px]")}>
+        <BreadCrumb items={breadcrumbItems} />
+      </div>
+      {isFetching && <Spinner />}
+      {!isFetching && data?.data && (
+        <OrderForm initialData={data?.data} isEdit />
+      )}
+    </div>
+  );
 }
