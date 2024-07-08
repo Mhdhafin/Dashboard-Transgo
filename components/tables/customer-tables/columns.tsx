77a810cbdd35cde96613cd742dcafd7bf3f64875
arisdanang <arisdanang2810@gmail.com>
2024-07-08 21:50:50 +0700

    fix: add status column

--- components/tables/customer-tables/columns.tsx
+++ components/tables/customer-tables/columns.tsx
@@ -4,6 +4,7 @@ import { CellAction } from "./cell-action";
 import { User } from "@/constants/data";
 import { Checkbox } from "@/components/ui/checkbox";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 export const columns: ColumnDef<any>[] = [
   // {
@@ -45,6 +46,24 @@ export const columns: ColumnDef<any>[] = [
     accessorKey: "email",
     header: "Email",
     cell: ({ row }) => <span>{row.original.email ?? "-"}</span>,
+  },
+  {
+    accessorKey: "status",
+    header: () => (
+      <span className="text-sm font-semibold text-neutral-700">Status</span>
+    ),
+    cell: ({ row }) => (
+      <span
+        className={cn(
+          " px-2 py-1 rounded-full",
+          row?.original?.status === "pending"
+            ? "bg-red-100 text-red-500"
+            : "bg-green-100 text-green-500",
+        )}
+      >
+        {row?.original?.status}
+      </span>
+    ),
   },
   {
     id: "actions",
