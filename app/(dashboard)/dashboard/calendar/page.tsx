a71cf7cb17ea7200dadf46bd73287a889148a494
Arya Perdana <arya.perdana@happy5.co>
2024-08-03 14:16:44 +0700

    feat: add calendar page

--- app/(dashboard)/dashboard/calendar/page.tsx
+++ app/(dashboard)/dashboard/calendar/page.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Metadata } from "next";
+
+import BreadCrumb from "@/components/breadcrumb";
+import { Heading } from "@/components/ui/heading";
+
+const BREAD_CRUMB_ITEMS = [{ title: "Calendar", link: "/dashboard/calendar" }];
+
+export const metadata: Metadata = {
+  title: "Calendar | Transgo",
+  description: "Calendar page",
+};
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="flex-1 space-y-4  p-4 md:p-8 pt-6">
+      <BreadCrumb items={BREAD_CRUMB_ITEMS} />
+      <Heading title="Calendar" />
+    </div>
+  );
 };
 
 export default page;
