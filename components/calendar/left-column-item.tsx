8b3d93738e356af797329d6a703ff5a7dc76fab4
Arya Perdana <arya.perdana@happy5.co>
2024-08-04 16:32:31 +0700

    feat(fleet): add tooltip

--- components/calendar/left-column-item.tsx
+++ components/calendar/left-column-item.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Tooltip from "./tooltip";
 
 const LeftColumnItem = ({
   vehicle,
@@ -8,20 +9,22 @@ const LeftColumnItem = ({
   isLast: boolean;
 }) => {
   return (
-    <div
-      className={`w-full h-[64px] border-r border-b border-neutral-200 ${
-        isLast ? "border-b-0" : ""
-      }`}
-    >
-      <div className="flex flex-col gap-1 py-[11px] px-[20px]">
-        <p className="text-neutral-900 font-medium text-[16px] leading-6">
-          {vehicle.name}
-        </p>
-        <p className="text-neutral-700 font-medium text-[14px] leading-[14px]">
-          {vehicle.location}
-        </p>
+    <Tooltip type="fleet">
+      <div
+        className={`w-full h-[64px] border-r border-b border-neutral-200 ${
+          isLast ? "border-b-0" : ""
+        }`}
+      >
+        <div className="flex flex-col gap-1 py-[11px] px-[20px]">
+          <p className="text-neutral-900 font-medium text-[16px] leading-6">
+            {vehicle.name}
+          </p>
+          <p className="text-neutral-700 font-medium text-[14px] leading-[14px]">
+            {vehicle.location}
+          </p>
+        </div>
       </div>
-    </div>
+    </Tooltip>
   );
 };
 
