b0b1b4cb630eea9c8343369f23bc3c34b66f2c48
Arya Perdana <arya.perdana@happy5.co>
2024-08-04 18:25:01 +0700

    refactor: separate component

--- components/calendar/calendar.tsx
+++ components/calendar/calendar.tsx
@@ -7,8 +7,8 @@ import useCalendarViewStore from "@/hooks/components/useCalendarViewStore";
 
 import LeftColumn from "./left-column";
 import Header from "./header";
-import Tooltip from "./tooltip";
 import Spinner from "../spinner";
+import Grid from "./grid";
 
 const Calendar = () => {
   const { calendarData, isFetching } = useCalendarViewStore({
@@ -46,17 +46,6 @@ const Calendar = () => {
     }
   }, [today]);
 
-  const getDayOffset = (date: string) => {
-    return dates.findIndex((d) => d.format("YYYY-MM-DD") === date);
-  };
-
-  const getTimeOffset = (startTime, endTime) => {
-    const start = startTime;
-    const end = endTime;
-    const totalHours = end.diff(start, "hour", true);
-    return totalHours;
-  };
-
   if (isFetching) {
     return <Spinner className="w-6 h-6" />;
   }
@@ -72,70 +61,7 @@ const Calendar = () => {
 
           <div className="flex-1">
             <Header dates={dates} />
-            <div>
-              {calendarData.map((vehicle, rowIndex) => (
-                <div key={rowIndex} className="flex relative">
-                  {dates.map((date) => {
-                    const isCurrentDate = date.format("YYYY-MM-DD") === today;
-                    const isLast = rowIndex === calendarData.length - 1;
-
-                    return (
-                      <div
-                        key={date.format("YYYY-MM-DD")}
-                        className={`relative border-r last:border-r-0 border-b border-gray-300 first:border-l-0 h-[64px] p-[12px] w-16 ${
-                          isLast ? "border-b-0" : ""
-                        }`}
-                        data-date={date.format("YYYY-MM-DD")}
-                      >
-                        {isCurrentDate && (
-                          <div className="absolute inset-0 flex items-center justify-center">
-                            <div className="w-0.5 h-full bg-blue-600"></div>
-                          </div>
-                        )}
-                      </div>
-                    );
-                  })}
-                  {vehicle.usage.map((usage, usageIndex) => {
-                    const startTime = usage.start;
-                    const endTime = usage.end;
-                    const startOffset = getDayOffset(
-                      startTime.format("YYYY-MM-DD"),
-                    );
-                    // const endOffset = getDayOffset(
-                    //   endTime.format("YYYY-MM-DD"),
-                    // );
-                    const totalHours = getTimeOffset(startTime, endTime);
-                    const dayWidth = 64;
-                    const boxHeight = 40;
-                    // const totalDays = endOffset - startOffset + 1;
-                    const width = (totalHours / 24) * dayWidth;
-
-                    return (
-                      <div
-                        className="absolute hover:border hover:border-green-900 bg-green-200 rounded-lg"
-                        key={usageIndex}
-                        style={{
-                          top: 12,
-                          left:
-                            startOffset * dayWidth +
-                            (startTime.hour() / 24) * dayWidth,
-                          width: width,
-                          height: boxHeight,
-                        }}
-                      >
-                        <Tooltip type="date">
-                          <div className="flex p-[10px] items-center justify-center h-full w-full">
-                            <span className="truncate leading-5 font-medium text-[12px] text-green-900">
-                              {usage.title}
-                            </span>
-                          </div>
-                        </Tooltip>
-                      </div>
-                    );
-                  })}
-                </div>
-              ))}
-            </div>
+            <Grid dates={dates} data={calendarData} />
           </div>
         </div>
       </div>
---
+++ components/calendar/grid.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+import dayjs from "dayjs";
+
+import Tooltip from "./tooltip";
+
+const Grid = ({ dates, data }: { dates: dayjs.Dayjs[] }) => {
+  const today = dayjs().format("YYYY-MM-DD");
+
+  const getDayOffset = (date: string) => {
+    return dates.findIndex((d) => d.format("YYYY-MM-DD") === date);
+  };
+
+  const getTimeOffset = (startTime, endTime) => {
+    const start = startTime;
+    const end = endTime;
+    const totalHours = end.diff(start, "hour", true);
+    return totalHours;
+  };
+
+  return (
+    <>
+      {data.map((vehicle, rowIndex) => (
+        <div key={rowIndex} className="flex relative">
+          {dates.map((date) => {
+            const isCurrentDate = date.format("YYYY-MM-DD") === today;
+            const isLast = rowIndex === data.length - 1;
+
+            return (
+              <div
+                key={date.format("YYYY-MM-DD")}
+                className={`relative border-r last:border-r-0 border-b border-gray-300 first:border-l-0 h-[64px] p-[12px] w-16 ${
+                  isLast ? "border-b-0" : ""
+                }`}
+                data-date={date.format("YYYY-MM-DD")}
+              >
+                {isCurrentDate && (
+                  <div className="absolute inset-0 flex items-center justify-center">
+                    <div className="w-0.5 h-full bg-blue-600"></div>
+                  </div>
+                )}
+              </div>
+            );
+          })}
+          {vehicle.usage.map((usage, usageIndex) => {
+            const startTime = usage.start;
+            const endTime = usage.end;
+            const startOffset = getDayOffset(startTime.format("YYYY-MM-DD"));
+            // const endOffset = getDayOffset(
+            //   endTime.format("YYYY-MM-DD"),
+            // );
+            const totalHours = getTimeOffset(startTime, endTime);
+            const dayWidth = 64;
+            const boxHeight = 40;
+            // const totalDays = endOffset - startOffset + 1;
+            const width = (totalHours / 24) * dayWidth;
+
+            return (
+              <div
+                className="absolute hover:border hover:border-green-900 bg-green-200 rounded-lg"
+                key={usageIndex}
+                style={{
+                  top: 12,
+                  left:
+                    startOffset * dayWidth + (startTime.hour() / 24) * dayWidth,
+                  width: width,
+                  height: boxHeight,
+                }}
+              >
+                <Tooltip type="date">
+                  <div className="flex p-[10px] items-center justify-center h-full w-full">
+                    <span className="truncate leading-5 font-medium text-[12px] text-green-900">
+                      {usage.title}
+                    </span>
+                  </div>
+                </Tooltip>
+              </div>
+            );
+          })}
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Grid;
