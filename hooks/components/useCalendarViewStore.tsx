9095ef4373bc388c06e17500fc963a3a3dc9ebc4
Arya Perdana <arya.perdana@happy5.co>
2024-08-05 12:09:22 +0700

    feat: add photo

--- hooks/components/useCalendarViewStore.tsx
+++ hooks/components/useCalendarViewStore.tsx
@@ -28,8 +28,7 @@ const useCalendarViewStore = (filter?: any) => {
     name: item.name,
     location: item.location.location,
     price: formatRupiah(item.price),
-    image:
-      "https://images.unsplash.com/photo-1515569067071-ec3b51335dd0?q=80&w=2738&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    image: item.photo?.photo,
     usage: item.orders.map((order: any) => ({
       id: order.id,
       start: dayjs(order.start_date).tz("Asia/Jakarta"),
