8b5fe5f959acd5564c00e588a9929811ea3523e3
Arya Perdana <arya.perdana@happy5.co>
2024-08-19 22:43:35 +0700

    feat: handle url access by role

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,41 @@
 // https://next-auth.js.org/configuration/nextjs#middleware
 // https://nextjs.org/docs/app/building-your-application/routing/middleware
 
-export { default } from "next-auth/middleware";
+import { Token } from "next-auth";
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const token = (await getToken({ req })) as Token;
+  const { pathname } = req.nextUrl;
+
+  if (!token || !token.user) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  const role = token?.user?.role || "admin";
+
+  const ownerRoutes = [
+    "/dashboard/calendar",
+    "/dashboard/fleets",
+    { path: "/dashboard/orders/:orderId/detail", exact: true },
+  ];
+
+  const isMatch = ownerRoutes.some((route) => {
+    if (typeof route === "string") {
+      return pathname.startsWith(route);
+    } else if (route.exact) {
+      const regex = new RegExp(`^${route.path.replace(":orderId", "[^/]+")}$`);
+      return regex.test(pathname);
+    }
+    return false;
+  });
+
+  if (role === "owner" && !isMatch) {
+    return NextResponse.redirect(new URL("/dashboard/calendar", req.url));
+  }
+
+  return NextResponse.next();
+}
 
 export const config = { matcher: ["/dashboard/:path*"] };
