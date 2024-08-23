99945c3068df731787158889cef37ee805f82d77
Arya Perdana <arya.perdana@happy5.co>
2024-08-23 19:44:50 +0700

    feat: add recap table

---
+++ components/tables/recap-tables/recap-table.tsx
@@ -0,0 +1,154 @@
+"use client";
+
+import React from "react";
+
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { formatRupiah } from "@/lib/utils";
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+export default function RecapTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <>
+      <ScrollArea className="rounded-md border border-solid max-h-[calc(100vh-200px)]">
+        <Table className="w-full overflow-x-auto">
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id} className="bg-slate-200">
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead
+                      key={header.id}
+                      className={`${
+                        header.column.columnDef.meta?.centerHeader
+                          ? "!text-center"
+                          : ""
+                      } ${
+                        header.column.columnDef.meta?.stickyColumn
+                          ? "sticky right-0 z-50"
+                          : ""
+                      }`}
+                    >
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  className="cursor-pointer hover:bg-gray-100 transition-colors duration-200 ease-in-out"
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => {
+                    return (
+                      <TableCell
+                        key={cell.id}
+                        className={`h-[48px] ${
+                          cell.column.columnDef.meta?.centerHeader
+                            ? "!text-center"
+                            : ""
+                        } ${
+                          cell.column.columnDef.meta?.stickyColumn
+                            ? "sticky right-0 z-50"
+                            : ""
+                        }`}
+                      >
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext(),
+                        )}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  Tidak ada data yang dapat ditampilkan.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+          <TableFooter>
+            <TableRow className="!border-b-0 border-solid border-t">
+              <TableCell
+                colSpan={6}
+                className="text-center h-[64px] bg-white text-neutral-700 text-sm font-semibold"
+              >
+                Jumlah
+              </TableCell>
+              <TableCell
+                colSpan={1}
+                className="text-center h-[64px] bg-white text-neutral-700 text-sm font-bold"
+              >
+                0 Hari
+              </TableCell>
+              <TableCell
+                colSpan={1}
+                className="text-center h-[64px] bg-white text-neutral-700 text-sm font-bold"
+              >
+                {formatRupiah(0)}
+              </TableCell>
+              <TableCell
+                colSpan={1}
+                className="text-center h-[64px] bg-white text-neutral-700 text-sm font-bold"
+              >
+                {formatRupiah(0)}
+              </TableCell>
+              <TableCell
+                colSpan={1}
+                className="text-center border-solid  h-[64px] bg-white text-neutral-700 text-sm font-bold"
+              >
+                {formatRupiah(0)}
+              </TableCell>
+            </TableRow>
+          </TableFooter>
+        </Table>
+        <ScrollBar orientation="vertical" />
+      </ScrollArea>
+    </>
+  );
+}
