1534a486ed960b40b52d4b9ed1f41206fb3ca2be
Arya Perdana <arya.perdana@happy5.co>
2024-08-24 20:44:25 +0700

    feat: add hook recaps store

--- hooks/api/useRecaps.ts
+++ hooks/api/useRecaps.ts
@@ -1,23 +1,18 @@
-import { useInfiniteQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import useAxiosAuth from "../axios/use-axios-auth";
 import { compact } from "lodash";
 
 export const useGetRecaps = (params?: Record<string, any>) => {
   const axiosAuth = useAxiosAuth();
 
-  const getRecaps = (pageParam = "1") => {
+  const getRecaps = () => {
     return axiosAuth.get("/ledgers/recaps", {
-      params: {
-        ...params,
-        page: pageParam,
-      },
+      params,
     });
   };
 
-  return useInfiniteQuery({
+  return useQuery({
     queryKey: compact(["fleets", "calendar"]),
-    queryFn: ({ pageParam }) => getRecaps(pageParam),
-    initialPageParam: undefined,
-    getNextPageParam: (lastPage) => lastPage.data.pagination?.next_page,
+    queryFn: getRecaps,
   });
 };
--- hooks/components/useRecapsStore.tsx
+++ hooks/components/useRecapsStore.tsx
@@ -1,9 +1,48 @@
 import { useGetRecaps } from "../api/useRecaps";
 
-const useRecapsStore = (params?: any) => {
-  const { data } = useGetRecaps({ ...params });
+interface IFleet {
+  color: string | null;
+  id: number;
+  name: string;
+}
 
-  return {};
+interface IItems {
+  id: number;
+  status: string;
+  update_at: string;
+  created_at: string;
+  date: string;
+  duration: number;
+  credit_amount: number | null;
+  debit_amount: number | null;
+  description: string | null;
+  fleet: IFleet;
+}
+
+export interface ITotal {
+  debit: 0;
+  credit: 0;
+  duration: 0;
+  commission: 0;
+}
+
+const useRecapsStore = (params?: any) => {
+  const { data: recaps, isFetching } = useGetRecaps({ ...params });
+
+  const items: IItems[] = recaps?.data?.items || [];
+  const total: ITotal = recaps?.data?.total
+    ? {
+        ...recaps?.data?.total,
+        commission:
+          recaps?.data?.total?.debit || 0 - recaps?.data?.total?.credit || 0,
+      }
+    : { debit: 0, credit: 0, duration: 0, commission: 0 };
+
+  return {
+    items,
+    total,
+    isFetching,
+  };
 };
 
 export default useRecapsStore;
