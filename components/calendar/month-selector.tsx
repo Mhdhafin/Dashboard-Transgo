743f45345c87bc8a6e184fccbb2a66a7b91d9b5e
Arya Perdana <arya.perdana@happy5.co>
2024-08-31 01:43:47 +0700

    refactor: remove unused import

--- components/calendar/month-selector.tsx
+++ components/calendar/month-selector.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 
 import { Button } from "../ui/button";
