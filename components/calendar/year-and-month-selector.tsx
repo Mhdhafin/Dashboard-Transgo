9472ab7b0b9ec8ee6e39a13053ac21dab7ea62a2
Muhammad Jamil <mjamil1339@gmail.com>
2024-09-04 15:06:11 +0700

    feat: add period filter and adjust layouting in request tasks

--- components/calendar/year-and-month-selector.tsx
+++ components/calendar/year-and-month-selector.tsx
@@ -4,15 +4,18 @@ import DateRangeSelector from "./date-range-selector";
 import MonthSelector from "./month-selector";
 import YearSelector from "./year-selector";
 import InputSearch from "./input-search";
+import InputType from "./input-type";
 
 const YearAndMonthSelector = ({
   withDateRange = false,
   withSearch = false,
+  withType = false,
 }) => {
   return (
     <div className="flex items-center gap-[10px]">
       {withSearch && <InputSearch />}
       {withDateRange && <DateRangeSelector />}
+      {withType && <InputType />}
       <MonthSelector />
       <YearSelector />
     </div>
